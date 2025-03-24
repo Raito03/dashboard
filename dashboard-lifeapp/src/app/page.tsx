d98d8634d581e44c47eb1827bdc6b4dc503b6cf8
Raito03 <anugraha0606@hotmail.com>
2025-03-24 16:48:57 +0530

    version 3.2.1

--- dashboard-lifeapp/src/app/page.tsx
+++ dashboard-lifeapp/src/app/page.tsx
@@ -51,8 +51,8 @@ import { Sidebar } from '@/components/ui/sidebar';
 import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
-// const api_startpoint = 'https://lifeapp-api-vv1.vercel.app'
-const api_startpoint = 'http://127.0.0.1:5000'
+const api_startpoint = 'https://lifeapp-api-vv1.vercel.app'
+// const api_startpoint = 'http://127.0.0.1:5000'
 
 
 import { format } from 'date-fns';
